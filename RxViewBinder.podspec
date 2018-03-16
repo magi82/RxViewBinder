62c7c894cb99a11cf32a9c535c7eb82e8662107d
devmagi82 <devmagi82@gmail.com>
2018-03-16 23:58:33 +0900

    Add initial project.

---
+++ RxViewBinder.podspec
@@ -0,0 +1,17 @@
+Pod::Spec.new do |s|
+  s.name             = 'RxViewBinder'
+  s.version          = '0.1.0'
+  s.summary          = 'RxViewBinder is a simple one-way architecture.'
+  s.description      = <<-DESC
+RxViewBinder is a one-way architecture using Reactive Extension.
+                       DESC
+  s.homepage         = 'https://github.com/magi82/RxViewBinder'
+  s.license          = { :type => 'MIT', :file => 'LICENSE' }
+  s.author           = { 'magi82' => 'devmagi82@gmail.com' }
+  s.source           = { :git => 'https://github.com/magi82/RxViewBinder.git', :tag => s.version.to_s }
+  s.ios.deployment_target = '9.0'
+  s.source_files = 'Sources/*.swift'
+  s.dependency 'RxSwift', '~> 4.0'
+  s.dependency 'RxCocoa', '~> 4.0'
+  s.swift_version = '4.0'
+end
---
+++ Sources/ViewBindable.swift
@@ -0,0 +1,91 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2018 ByungKook Hwang (https://magi82.github.io)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import RxSwift
+import RxCocoa
+
+public protocol ViewBindable: class {
+  associatedtype Command
+  associatedtype Action
+  associatedtype State
+  
+  var disposeBag: DisposeBag { get set }
+  var command: PublishRelay<Command> { get }
+  var action: Action { get }
+  var state: State { get }
+  
+  func binding(command: Command)
+}
+
+// MARK: - disposeBag
+
+private var disposeBagKey: String = "disposeBag"
+extension ViewBindable {
+  
+  public var disposeBag: DisposeBag {
+    get {
+      if let value = objc_getAssociatedObject(self,
+                                              &disposeBagKey) as? DisposeBag {
+        return value
+      }
+      
+      let disposeBag = DisposeBag()
+      
+      objc_setAssociatedObject(self,
+                               &disposeBagKey,
+                               disposeBag,
+                               objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+      return disposeBag
+    }
+    
+    set {
+      objc_setAssociatedObject(self,
+                               &disposeBagKey,
+                               newValue,
+                               objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    }
+  }
+}
+
+// MARK: - command
+
+private var commandKey: String = "command"
+extension ViewBindable {
+  
+  public var command: PublishRelay<Command> {
+    get {
+      if let value = objc_getAssociatedObject(self,
+                                              &commandKey) as? PublishRelay<Command> {
+        return value
+      }
+      
+      let command: PublishRelay<Command> = PublishRelay()
+      command.subscribe(onNext: binding).disposed(by: disposeBag)
+      
+      objc_setAssociatedObject(self,
+                               &commandKey,
+                               command,
+                               objc_AssociationPolicy.OBJC_ASSOCIATION_ASSIGN)
+      return command
+    }
+  }
+}
