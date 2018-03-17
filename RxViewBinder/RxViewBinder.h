dcbc6275d8d15553cc666c15a1319bd0368440ff
byungkook hwang <bkhwang82@gmail.com>
2018-03-17 17:42:24 +0900

    setting carthage.

---
+++ RxViewBinder/RxViewBinder.h
@@ -0,0 +1,19 @@
+//
+//  RxViewBinder.h
+//  RxViewBinder
+//
+//  Created by 황병국 on 2018. 3. 17..
+//  Copyright © 2018년 magi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxViewBinder.
+FOUNDATION_EXPORT double RxViewBinderVersionNumber;
+
+//! Project version string for RxViewBinder.
+FOUNDATION_EXPORT const unsigned char RxViewBinderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxViewBinder/PublicHeader.h>
+
+
